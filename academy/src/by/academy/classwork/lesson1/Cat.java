1e4e5c99fe6d44d8006846c80ce63776ef9509d3
CraM-zy <64412012+CraM-zy@users.noreply.github.com>
2020-10-10 13:57:09 +0300

    Update Cat.java

--- academy/src/by/academy/classwork/lesson1/Cat.java
+++ academy/src/by/academy/classwork/lesson1/Cat.java
@@ -11,17 +11,17 @@ public class Cat
 	}
 	public void sleep() 
 	{
-		System.out.println("Кот спит");
+		System.out.println("РљРѕС‚ СЃРїРёС‚");
 	}
 	public void eat() 
 	{
-		System.out.println("Кот ест");
+		System.out.println("РљРѕС‚ РµСЃС‚");
 	}
 	public void walk() 
 	{
-		System.out.println("Кот гуляет");
+		System.out.println("РљРѕС‚ РіСѓР»СЏРµС‚");
 	}
-	public Cat() {} //конструктор по умолчанию
+	public Cat() {} //РєРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
 	public Cat(String nickname, int age)
 	{
 		this.nickname=nickname;
