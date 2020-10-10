875f3d7483442a18729be379a38eb0f60df5f48b
CraM-zy <64412012+CraM-zy@users.noreply.github.com>
2020-10-10 15:45:30 +0300

    test

--- academy/src/by/academy/classwork/lesson1/Cat.java
+++ academy/src/by/academy/classwork/lesson1/Cat.java
@@ -1,30 +1,30 @@
 package by.academy.classwork.lesson1;
 
-public class Cat 
-{
+public class Cat {
 	int age;
 	String nickname;
-	public void grow(int addage) 
-	{
-		age=age+addage;
+
+	public void grow(int addage) {
+		age = age + addage;
 		System.out.println("Now the age of second cat is:" + age);
 	}
-	public void sleep() 
-	{
+
+	public void sleep() {
 		System.out.println("Кот спит");
 	}
-	public void eat() 
-	{
+
+	public void eat() {
 		System.out.println("Кот ест");
 	}
-	public void walk() 
-	{
+
+	public void walk() {
 		System.out.println("Кот гуляет");
 	}
-	public Cat() {} //конструктор по умолчанию
-	public Cat(String nickname, int age)
-	{
-		this.nickname=nickname;
+
+	public Cat() {
+	} // конструктор по умолчанию
+
+	public Cat(String nickname, int age) {
+		this.nickname = nickname;
 	}
 }
-
